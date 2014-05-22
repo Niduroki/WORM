4d55c6ef01c9366f5b12e5259f7c28acbf628d8b
kondou <christopher@schaepers.it>
2014-05-22 11:26:30 +0200

    Put player serialize in Player.java

--- src/de/hhu/propra14/team101/GameSaves.java
+++ src/de/hhu/propra14/team101/GameSaves.java
@@ -49,7 +49,7 @@ public class GameSaves extends AbstractSaver {
     private Object[] serializePlayerArray(ArrayList<Player> playerArray) {
         Object[] result = new Object[playerArray.size()];
         for (int i=0; i<playerArray.size(); i++) {
-            result[i] = this.serializePlayer(playerArray.get(i));
+            result[i] =  playerArray.get(i).serialize();
         }
         return result;
     }
@@ -57,32 +57,8 @@ public class GameSaves extends AbstractSaver {
     private ArrayList<Player> deserializePlayerArray(ArrayList<Map> input) {
         ArrayList<Player> result = new ArrayList<Player>();
         for (int i=0; i<input.size(); i++) {
-            result.add(deserializePlayer(input.get(i)));
+            result.add(Player.deserialize(input.get(i)));
         }
         return result;
-    }
-
-    private Map serializePlayer(Player player) {
-        Map<String, Object> map = new HashMap<String, Object>();
-        map.put("type", player.type);
-        map.put("current_worm", player.currentWorm);
-        ArrayList<Map> wormArray = new ArrayList<Map>();
-        for (int i=0; i<player.wormList.size(); i++) {
-            wormArray.add(player.wormList.get(i).serialize());
-        }
-        map.put("worm_array", wormArray);
-        return map;
-    }
-
-    private Player deserializePlayer(Map input) {
-        ArrayList<Map> rawWorms = new ArrayList<Map>();
-        rawWorms = (ArrayList<Map>) input.get("worm_array");
-        ArrayList<Worm> wormList = new ArrayList<>();
-        for (int i=0; i<rawWorms.size(); i++) {
-            wormList.add(Worm.deserialize(rawWorms.get(i)));
-        }
-        Player player = new Player(wormList, (String)input.get("type"));
-        player.currentWorm = (Integer) input.get("current_worm");
-        return player;
     }
 }
--- src/de/hhu/propra14/team101/Player.java
+++ src/de/hhu/propra14/team101/Player.java
@@ -4,6 +4,8 @@ package de.hhu.propra14.team101;
 import javafx.scene.paint.Color;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Player {
 
@@ -42,5 +44,30 @@ public class Player {
         } else {
             this.currentWorm += 1;
         }
+    }
+
+    public Map serialize() {
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put("type", this.type);
+        map.put("current_worm", this.currentWorm);
+        map.put("color", this.color);
+        ArrayList<Map> wormArray = new ArrayList<Map>();
+        for (int i=0; i<this.wormList.size(); i++) {
+            wormArray.add(this.wormList.get(i).serialize());
+        }
+        map.put("worm_array", wormArray);
+        return map;
+    }
+
+    public static Player deserialize(Map input) {
+        ArrayList<Map> rawWorms = (ArrayList<Map>) input.get("worm_array");
+        ArrayList<Worm> wormList = new ArrayList<>();
+        for (int i=0; i<rawWorms.size(); i++) {
+            wormList.add(Worm.deserialize(rawWorms.get(i)));
+        }
+        Player player = new Player(wormList, (String)input.get("type"));
+        player.currentWorm = (Integer) input.get("current_worm");
+        player.color = (Color) input.get("color");
+        return player;
     }
 }
