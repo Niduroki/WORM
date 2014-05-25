6f9fb88f51288c76b427278001acdb2d7e1e33da
MariannaNaak <MariannaNaak@gmail.com>
2014-05-25 20:00:43 +0200

    image changed

--- src/de/hhu/propra14/team101/TerrainObjects/TriangleBuildingBlock.java
+++ src/de/hhu/propra14/team101/TerrainObjects/TriangleBuildingBlock.java
@@ -34,10 +34,10 @@ public class TriangleBuildingBlock extends AbstractTerrainObject {
     public void draw (GraphicsContext gc) {
 
         if (this.getSlopedLeft()) {
-            Image image = new Image("ground-triangle-left.png");
+            Image image = new Image("ground-triangle-right.png");
             gc.drawImage(image, this.xCoordinate*this.size, this.yCoordinate*this.size);
         } else {
-            Image image = new Image("ground-triangle-rigth.png");
+            Image image = new Image("ground-triangle-left.png");
             gc.drawImage(image, this.xCoordinate*this.size, this.yCoordinate*this.size);
         }
 
